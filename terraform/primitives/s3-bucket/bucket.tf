51c2b301a6f606d4dc6e5a250853a5e8d0c2ff75
Xianfeng Yuan <xianyuan@gmail.com>
2025-07-11 19:55:17 -0700

    bug fix

--- terraform/primitives/s3-bucket/bucket.tf
+++ terraform/primitives/s3-bucket/bucket.tf
@@ -35,7 +35,7 @@ resource "aws_s3_bucket_acl" "bucket" {
 }
 
 resource "aws_s3_bucket_versioning" "bucket" {
-  bucket = aws_s3_bucket.bucket
+  bucket = aws_s3_bucket.bucket.id
 
   versioning_configuration {
     status = "Enabled"
